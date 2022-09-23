b7cef25cb8c15b2f78565fbc27f91c1b3aceb374
oaadmin@protonmail.com <oaadmin@protonmail.com>
2022-09-23 05:33:05 +0000

    Edited Main, disclaimer_copy and disclaimer

--- client_code/disclaimer_copy.py
+++ client_code/disclaimer_copy.py
@@ -31,7 +31,7 @@ class disclaimer_copy(disclaimer_copyTemplate):
     
       a = ethereum.request({
                 'method': 'wallet_switchEthereumChain',
-                'params': [{ "chainId": '0x3AD' }]
+                'params': [{ "chainId": '0x1' }]
             })
       anvil.js.await_promise(a)
       anvil.js.window.location.replace("https://go.maximus.cash")
